62697ae4bc26e2e60042767413fc02e1948f85af
lI15SO0 <svvivi@outlook.com>
2026-05-31 03:32:39 +0800

    change(nix_basic_setting): move substituters into extra-substituters

--- system/nix_basic_setting.nix
+++ system/nix_basic_setting.nix
@@ -1,25 +1,32 @@
 { lib, ... } :
 {
-  # Nix settings.
-  nix={
-    settings= {
-      substituters = [ "https://mirrors.cernet.edu.cn/nix-channels/store" ];
-      experimental-features = [ "nix-command" "flakes" ];
-	  extra-substituters = [ "https://noctalia.cachix.org" ];
-	  extra-trusted-public-keys = [ "noctalia.cachix.org-1:pCOR47nnMEo5thcxNDtzWpOxNFQsBRglJzxWPp3dkU4=" ];
-    };
-    gc = {
-      automatic = lib.mkDefault true;
-      dates = lib.mkDefault "weekly";
-      options = lib.mkDefault "--delete-older-than 7d";
-    };
-  };
+	# Nix settings.
+	nix = {
+		settings = {
+			# substituters = lib.mkForce [
+			# 	"https://mirrors.cernet.edu.cn/nix-channels/store" 
+			# 	"https://cache.nixos.org"
+			# ];
+			experimental-features = [ "nix-command" "flakes" ];
+			extra-substituters = [ "https://mirrors.cernet.edu.cn/nix-channels/store" "https://noctalia.cachix.org" ];
+			extra-trusted-public-keys = [
+				"noctalia.cachix.org-1:pCOR47nnMEo5thcxNDtzWpOxNFQsBRglJzxWPp3dkU4=" 
+				"cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
+			];
+		};
 
-  environment.sessionVariables = rec {
-	EDITOR="nvim" ;
-  };
+		gc = {
+			automatic = lib.mkDefault true;
+			dates = lib.mkDefault "weekly";
+			options = lib.mkDefault "--delete-older-than 7d";
+		};
+	};
 
-  # Locale settings
-  time.timeZone = "Asia/Shanghai";
-  i18n.defaultLocale = "zh_CN.UTF-8";
+	environment.sessionVariables = rec {
+		EDITOR="nvim" ;
+	};
+
+	# Locale settings
+	time.timeZone = "Asia/Shanghai";
+	i18n.defaultLocale = "zh_CN.UTF-8";
  }
