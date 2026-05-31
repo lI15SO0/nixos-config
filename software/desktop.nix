1426bb57909f4a1a0f6a5529bca99e64a3d22330
lI15SO0 <svvivi@outlook.com>
2026-06-01 01:58:52 +0800

    add(software/desktop): add pcmanfm; enable usb driver auto mount.

--- software/desktop.nix
+++ software/desktop.nix
@@ -11,6 +11,10 @@
 
 	services = {
 		displayManager.ly.enable = true;
+
+		gvfs.enable = true; # Enable usb driver auto mount.
+		devmon.enable = true;
+		udisks2.enable = true;
 	};
 
 	environment.systemPackages = with pkgs; [
@@ -22,5 +26,9 @@
 		wl-clipboard
 
 		vlc
+
+		pcmanfm
+		lxmenu-data
+		shared-mime-info
 	];
 }
