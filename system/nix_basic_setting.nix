dbb2348bae989cff6bf8046d125ef8ba5a863b73
lI15SO0 <svvivi@outlook.com>
2026-06-04 10:36:19 +0800

    change substituters

--- system/nix_basic_setting.nix
+++ system/nix_basic_setting.nix
@@ -3,14 +3,17 @@
 	# Nix settings.
 	nix = {
 		settings = {
-			# substituters = lib.mkForce [
-			# 	"https://mirrors.cernet.edu.cn/nix-channels/store" 
-			# 	"https://cache.nixos.org"
-			# ];
+			substituters = lib.mkForce [
+				"https://mirrors.cernet.edu.cn/nix-channels/store" 
+				"https://cache.nixos.org"
+			];
 			experimental-features = [ "nix-command" "flakes" ];
-			extra-substituters = [ "https://mirrors.cernet.edu.cn/nix-channels/store" "https://noctalia.cachix.org" ];
+			extra-substituters = [
+				# "https://mirrors.cernet.edu.cn/nix-channels/store"
+				# "https://noctalia.cachix.org" 
+			];
 			extra-trusted-public-keys = [
-				"noctalia.cachix.org-1:pCOR47nnMEo5thcxNDtzWpOxNFQsBRglJzxWPp3dkU4=" 
+				# "noctalia.cachix.org-1:pCOR47nnMEo5thcxNDtzWpOxNFQsBRglJzxWPp3dkU4=" 
 				"cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
 			];
 		};
